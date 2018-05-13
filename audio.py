f847fef1e8ce219942916e61c601ccee15efa76f
m45adiwinata <m45adiwinata@gmail.com>
2018-05-13 12:50:38 +0800

    perbaikan fft

--- audio.py
+++ audio.py
@@ -29,10 +29,15 @@ class AudioFile:
     def play(self):
         """ Play entire file """
         data = self.wf.readframes(chunk)
-        
         while data != '':
             self.stream.write(data)
             data = self.wf.readframes(chunk)
+
+        #save data to file
+        file = open("Text File/signal_data.txt","w")
+        for item in self.datas:
+            file.write("%s " % item)
+        file.close()
 
         #time domain
         plt.title("Wave-Time Domain")
@@ -42,20 +47,37 @@ class AudioFile:
         plt.show()
         
         #proses FFT
-        p = 20*np.log10(np.abs(np.fft.rfft(self.datas[:chunk*2])))/5
+        p = 20*np.log10(np.abs(np.fft.rfft(self.datas[:])))/5
         f = np.linspace(0, self.rates, len(p))
+        
+        #save FFT to file
+        file = open("Text File/fft_frequencies.txt","w")
+        i=0
+        for item in f:
+            file.write("%s) " % str(i+1))
+            file.write("%s\t:" % item)
+            file.write(" %s\n" % p[i])
+            i+=1
+        file.close()
+        
+        #frequency domain
         pl.plot(f, p)
         plt.title("Wave-Frequency Domain (FFT)")
         pl.xlabel("Frequency(Hz)")
         pl.ylabel("Power(dB)")
         pl.show()
-        
+
+        #proses finding peak
+        execfile("find_peak_cwt.py")
+
+        '''
         #proses Hamming Window
         A = fft(self.datas, chunk*2) / 25.5
         mag = np.abs(fftshift(A))
         freq = np.linspace(-0.5, 0.5, len(A))
         response = 20 * np.log10(mag)
         response = np.clip(response, -100, 100)
+        
         
         plt.plot(freq, response)
         plt.title("Frequency response of Hamming window")
@@ -65,7 +87,7 @@ class AudioFile:
         plt.axis('tight')
         plt.show()
         
-        '''
+        
         fval = []
         for i in range(len(f)-1):
             if f[i]!=f[i+1]:
@@ -83,18 +105,15 @@ class AudioFile:
                     fval.append(f[i])
         if len(f)==len(fval)+1:
             print("fft berhasil")
-        '''
-
-        
+        '''        
 
     def close(self):
         """ Graceful shutdown """ 
         self.stream.close()
         self.p.terminate()
 
-    
 
 # Usage example for pyaudio
-a = AudioFile("dog_bark4.wav")
+a = AudioFile("NewData/dog_bark4.wav")
 a.play()
 a.close()
